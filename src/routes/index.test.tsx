2b7a13923efb834f764374a669a15ddc974aa19b
Rob Smith <merlin@sleepingkingstudios.com>
2023-02-06 14:36:05 -0500

    Implement Resources generateResource().
    - Define Publishers resource.

--- src/routes/index.test.tsx
+++ src/routes/index.test.tsx
@@ -1,30 +1,26 @@
 import * as React from 'react';
 
 import '@testing-library/jest-dom';
+import { Route } from 'react-router-dom';
 
 import { ApplicationRoutes } from './index';
-import {
-  DemoPage,
-  HomePage,
-  LoginPage,
-  NotFoundPage,
-  UserPage,
-} from '../pages';
 import { shouldRenderAuthenticatedRoute } from '@test-helpers/routing';
 
-jest.mock('../pages');
-
-const mockDemoPage = DemoPage as jest.MockedFunction<typeof DemoPage>;
-const mockHomePage = HomePage as jest.MockedFunction<typeof HomePage>;
-const mockLoginPage = LoginPage as jest.MockedFunction<typeof LoginPage>;
-const mockNotFoundPage = NotFoundPage as jest.MockedFunction<typeof NotFoundPage>;
-const mockUserPage = UserPage as jest.MockedFunction<typeof UserPage>;
-
-mockDemoPage.mockImplementation(() => (<div id="page">Demo Page</div>))
-mockHomePage.mockImplementation(() => (<div id="page">Home Page</div>));
-mockLoginPage.mockImplementation(() => (<div id="page">Login Page</div>));
-mockNotFoundPage.mockImplementation(() => (<div id="page">Not Found Page</div>));
-mockUserPage.mockImplementation(() => (<div id="page">User Page</div>));
+jest.mock('@core/publishers', () => ({
+  routes: () => (
+    <Route
+      path="/publishers"
+      element={<div id="page">Publishers Routes</div>}
+    />
+  )
+}));
+jest.mock('@pages', () => ({
+  DemoPage: () => (<div id="page">Demo Page</div>),
+  HomePage: () => (<div id="page">Home Page</div>),
+  LoginPage: () => (<div id="page">Login Page</div>),
+  NotFoundPage: () => (<div id="page">Not Found Page</div>),
+  UserPage: () => (<div id="page">User Page</div>),
+}));
 
 describe('<ApplicationRoutes>', () => {
   shouldRenderAuthenticatedRoute(
@@ -53,5 +49,10 @@ describe('<ApplicationRoutes>', () => {
   shouldRenderAuthenticatedRoute(
     ApplicationRoutes,
     { content: 'User Page', at: '/user' },
+  );
+
+  shouldRenderAuthenticatedRoute(
+    ApplicationRoutes,
+    { content: 'Publishers Routes', at: '/publishers' },
   );
 });
