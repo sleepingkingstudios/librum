1c81a98718cd64c70b58dc5b71fe8dfcd8966492
Rob Smith <merlin@sleepingkingstudios.com>
2023-04-02 00:01:25 -0400

    Update UserUpdatePassword to use fetch-based API request.

--- src/pages/user/request.test.ts
+++ src/pages/user/request.test.ts
@@ -9,39 +9,41 @@ jest.mock('@api/request');
 const mockUseApiQuery =
   useApiQuery as jest.MockedFunction<typeof useApiQuery>;
 
-describe('UserPage request', () => {
-  const url = 'api/authentication/user';
-  const alerts = [
-    {
-      status: 'failure',
-      display: {
-        context: 'pages:user:request',
-        icon: faUser,
-        message: 'Unable to load current user.',
-        type: 'failure',
+describe('<UserPage /> request', () => {
+  describe('useGetUserQuery()', () => {
+    const url = 'api/authentication/user';
+    const alerts = [
+      {
+        status: 'failure',
+        display: {
+          context: 'pages:user:request',
+          icon: faUser,
+          message: 'Unable to load current user.',
+          type: 'failure',
+        },
       },
-    },
-    {
-      status: 'success',
-      dismiss: 'pages:user:request',
-    },
-  ];
-  const expected = {
-    alerts,
-    url,
-  };
+      {
+        status: 'success',
+        dismiss: 'pages:user:request',
+      },
+    ];
+    const expected = {
+      alerts,
+      url,
+    };
 
-  beforeEach(() => {
-    mockUseApiQuery.mockClear();
-  });
+    beforeEach(() => {
+      mockUseApiQuery.mockClear();
+    });
 
-  it('should be a function', () => {
-    expect(typeof useGetUserQuery).toBe('function');
-  });
+    it('should be a function', () => {
+      expect(typeof useGetUserQuery).toBe('function');
+    });
 
-  it('should configure the request', () => {
-    renderHook(() => useGetUserQuery());
+    it('should configure the request', () => {
+      renderHook(() => useGetUserQuery());
 
-    expect(mockUseApiQuery).toHaveBeenCalledWith(expected);
+      expect(mockUseApiQuery).toHaveBeenCalledWith(expected);
+    });
   });
 });
