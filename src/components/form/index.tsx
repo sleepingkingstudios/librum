bb83ed8716aeced8a2548e5d7d2b12099efc36ef
Rob Smith <merlin@sleepingkingstudios.com>
2023-04-02 18:12:17 -0400

    Remove LegacyApiForm component.

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -3,6 +3,5 @@ export { FormButton } from './button';
 export { FormField } from './field';
 export { Form } from './form';
 export { FormInput } from './input';
-export { LegacyApiForm } from './legacy-api-form';
 export { FormRow } from './row';
 export { FormSubmitButton } from './submit-button';
