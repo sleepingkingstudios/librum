8b8d00433a8600d3ed62d15242ea638e81297ab5
Rob Smith <merlin@sleepingkingstudios.com>
2023-06-26 23:55:22 -0400

    Remove legacy API controllers.
    - Remove legacy API controller actions.
    - Remove legacy serializers.

--- lib/serializers/json/sources/book_serializer.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Serializers::Json::Sources
-  # Serializes Sources::Books as JSON.
-  class BookSerializer < Serializers::Json::SourceSerializer
-    attribute :publication_date
-  end
-end
